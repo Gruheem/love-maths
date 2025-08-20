572023807a53c4a059c558955966e297ac677710
Gruheem <grahamsilcock91@gmail.com>
2025-08-20 18:14:27 +0100

    Add 'function displayAdditionQuestion'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,50 @@
 //Wait fo rht eDOM to finish loading before running the game
 //Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-            }
-        })
-
-    }
-    runGame("addition")
-})
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        runGame(gameType);
+      }
+    });
+  }
+  runGame("addition");
+});
 
 /**
  * The main game"loop", callede when the script is first loaded and after the user's answer has been processed
  */
-function runGame(gametype) {
+function runGame(gameType) {
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
 
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
-
-    if (gameType === "addition") {
-        displayAdditionQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
-    }
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+  }
 }
 
-function checkAnswer() {
+function checkAnswer() {}
 
-}
+function calculateCorrectAnswer() {}
 
-function calculateCorrectAnswer() {
+function incrementScore() {}
 
-}
-
-function incrementScore() {
-
-}
-
-function incrementWrongAnswer() {
-
-}
+function incrementWrongAnswer() {}
 
 function displayAdditionQuestion(operand1, operand2) {
-    
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "+";
 }
 
-function displaySutractionQuestion() {
+function displaySutractionQuestion() {}
 
-}
-
-function displayMultiplyQuestion() {
-
-}
+function displayMultiplyQuestion() {}
